d9b6bed6be1a014291fdbd3506c009876401d6f7
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 12:25:32 +0300

    basic chart structure

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,7 +1,9 @@
 const svg = d3.select("svg");
 
+//* Editing the svg values
 svg.attr("width", 800).attr("height", data.length * 150);
 
+//* Group
 const dataPoints = svg
 	.selectAll("g.data-point")
 	.data(data)
@@ -10,7 +12,7 @@ const dataPoints = svg
 	.attr("class", "data-point")
 	.attr("transform", (_, i) => `translate(0, ${i * 150})`);
 
-// text
+//* text
 dataPoints
 	.append("text")
 	.attr("x", 175)
@@ -23,3 +25,24 @@ dataPoints
 	.attr("y", 100)
 	.attr("class", "country")
 	.text((d) => d.country);
+
+//* Groups
+const months = dataPoints
+	.append("g")
+	.attr("class", "months")
+	.attr("transform", "translate(200, 0)");
+const monthGroups = months
+	.selectAll("g.month")
+	.data((d) => d.months)
+	.enter()
+	.append("g")
+	.attr("class", "month")
+	.attr("transform", (_, i) => `translate(${i * 50}, 0)`);
+
+monthGroups
+	.append("rect")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("width", 40)
+	.attr("height", 140)
+	.style("fill", "red");
