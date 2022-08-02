e66d22b4339d159981fa78911e446a29d6bc3f52
Clupai8o0 <clupaio4@gmail.com>
2022-08-02 12:18:42 +0300

    adding chart text

---
+++ js/heatmap.js
@@ -0,0 +1,18 @@
+const svg = d3.select("svg");
+
+svg.attr("width", 800).attr("height", data.length * 150);
+
+const dataPoints = svg
+	.selectAll("g.data-point")
+	.data(data)
+	.enter()
+	.append("g")
+	.attr("class", "data-point")
+	.attr("transform", (_, i) => `translate(0, ${i * 150})`);
+
+dataPoints
+	.append("text")
+	.attr("x", 175)
+	.attr("y", 70)
+	.attr("class", "city")
+	.text((d) => d.city);
